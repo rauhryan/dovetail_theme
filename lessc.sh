57c4df12465ac7010deac0a3b970b233fa8c744b
Ryan Rauh <rauh.ryan@gmail.com>
2011-09-21 13:29:06 -0500

    reorganizing things

--- lessc.sh
+++ lessc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
                                                        
-dotless/dotless.Compiler.exe -w styles/lib/bootstrap.less styles/bootstrap.css
+tools/dotless/dotless.Compiler.exe -w dovetail.less dovetail.css
